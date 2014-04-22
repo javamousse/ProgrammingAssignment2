e93da0a9ed4894e544a9945b86cadaf1ac5ad763
javamousse <elliott_langston@ml-20015863.wgbh.org>
2014-04-22 09:29:12 -0400

    cacheSolve with msg
    Newest version of the functions which actually caches the data and
    outputs a message if there isn't a current cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,41 @@
 ## The following two funcitons work together - with cacheSolve referencing makeCacheMatrix
-## to create a matrix x and return the inverse of matrix x
+## to create a matrix x and return the inverse of matrix x via cache
 
 ## The mackCacheMatrix creates a matrix with input of a matrix x
-## It then creates a list with two functions of 
+## It then creates a list with four functions of 
 ## set which sets the data in the matrix
-## and get which returns the data in the matrix
+## get which returns the data in the matrix
+## setInv which creates the inverse of matrix x
+## getInv which returns the data of the inverse of matrix x
 
 makeCacheMatrix<-function(x=matrix()){
-
+        inv<-NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                inv<<-NULL
         }
         get <- function() x
-
-        list(set = set, get = get)
+        setInv<-function(solve)inv <<- solve
+        getInv<-function()inv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## This function returns the inverse of matrix x by referencing the 
-## get() function created in makeCacheMatrix
+## cacheSolve creates a cache of the inverse of matrix x from the
+## makeCacheMatrix function by referencing the nested funcion
+## getInv. If there is no cache, it outputs a message relaying that it is
+## getting the cached data, then creates the inverse of matrix x
 
 cacheSolve <- function(x, ...) {
-        solve(x$get())
+        inv<-x$getInv()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data<-x$get()
+        inv<-solve(data,...)
+        x$setInv(inv)
+        inv
 }
