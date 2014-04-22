6a021bd323d3c3228ef2761983872e7f58dc6100
javamousse <elliott_langston@ml-20015863.wgbh.org>
2014-04-22 08:52:49 -0400

    Assignment 2
    A new version of cachematrix.R which completes the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two funcitons work together - with cacheSolve referencing makeCacheMatrix
+## to create a matrix x and return the inverse of matrix x
 
-## Write a short comment describing this function
+## The mackCacheMatrix creates a matrix with input of a matrix x
+## It then creates a list with two functions of 
+## set which sets the data in the matrix
+## and get which returns the data in the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix<-function(x=matrix()){
 
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+
+        list(set = set, get = get)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of matrix x by referencing the 
+## get() function created in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        solve(x$get())
 }
